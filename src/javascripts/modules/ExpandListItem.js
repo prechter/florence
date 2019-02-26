ed7e100b61f09c311926bd27acf029f87501dc55
Michael Prechter <m.r.prechter@gmail.com>
2019-02-26 16:08:56 -0500

    aria-pressed to aria-expanded, add debounce function

--- src/javascripts/modules/ExpandListItem.js
+++ src/javascripts/modules/ExpandListItem.js
@@ -1,18 +1,25 @@
+import { debounce } from 'utility/debounce'
+
 export default class ExpandListItem {
   constructor(el) {
     this.el = el
     this.tag = el.parentNode
     this.el.addEventListener('click', this.expand)
+
+    let resizeRate = debounce(this.resize, 250)
+    window.addEventListener('resize', resizeRate)
   }
 
   expand = () => {
-    let pressed = this.el.getAttribute('aria-pressed') === 'true'
-    if (this.tag.style.visibility === 'visible') {
-      this.tag.style.visibility = 'hidden'
-      this.el.setAttribute('aria-pressed', String('false'))
-    } else {
+    let isExpanded = this.el.getAttribute('aria-expanded') === 'true'
+    this.tag.style.visibility = isExpanded ? 'hidden' : 'visible'
+    this.el.setAttribute('aria-expanded', !isExpanded)
+  }
+
+  //for reverting all open or closed tags to visible when resizing browser window
+  resize = () => {
+    if (window.matchMedia('(max-width: 960px)').matches) {
       this.tag.style.visibility = 'visible'
-      this.el.setAttribute('aria-pressed', String(!pressed))
     }
   }
 }
---
+++ src/javascripts/utility/debounce.js
@@ -0,0 +1,24 @@
+//Source: https://davidwalsh.name/javascript-debounce-function
+
+export function debounce(func, wait, immediate) {
+  let timeout;
+
+  return function executedFunction() {
+    let context = this;
+    let args = arguments;
+	    
+    let later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+
+    let callNow = immediate && !timeout;
+	
+    clearTimeout(timeout);
+
+    timeout = setTimeout(later, wait);
+	
+    if (callNow) func.apply(context, args);
+  };
+};
+
