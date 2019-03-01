2f08d7fbea527b19cecb2510347426d9450405cf
Michael Prechter <m.r.prechter@gmail.com>
2019-03-01 15:05:35 -0500

    refactor ExpandList module to include aria-expanded, remove debounce.lodash

--- src/javascripts/modules/ExpandList.js
+++ src/javascripts/modules/ExpandList.js
@@ -1,21 +1,19 @@
-import debounce from 'lodash.debounce'
-
 export default class ExpandList {
   constructor(el) {
     this.el = el
     this.tagArray = Array.from(this.el.querySelectorAll('.tag-list__item'))
     this.btnArray = Array.from(this.el.querySelectorAll('.tag-list__btn'))
-
+    this.mq = window.matchMedia('(max-width: 959px)')
     this.bindEvents()
   }
-
+  
   bindEvents() {
     this.btnArray.forEach(btn => btn.addEventListener('click', this.expand))
-    window.addEventListener('resize', debounce(this.resize, 250))
+    this.mq.addListener(this.resize)
   }
 
   //for expanding tag-list__items when clicking tag-list__btns
-  expand = (e) => {
+  expand = e => {
     let isExpanded = e.currentTarget.getAttribute('aria-expanded') === 'true'
     e.currentTarget.setAttribute('aria-expanded', !isExpanded)
     
@@ -23,10 +21,12 @@ export default class ExpandList {
     this.tagArray[btnIndex].style.visibility = isExpanded ? 'hidden' : 'visible'
   }
 
-  //for reverting all open or closed tags to visible when resizing browser window
-  resize = () => {
-    if (window.matchMedia('(max-width: 960px)').matches) {
+ //for reverting all open or closed tags to visible when resizing browser window
+  resize = e => {
+    if (e.matches) {
       this.tagArray.forEach(tag => tag.style.visibility = 'visible')
+    } else {
+      this.btnArray.forEach(btn => btn.setAttribute('aria-expanded', 'true'))
     }
   }
 }
